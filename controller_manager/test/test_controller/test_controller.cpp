e4adc8bef435cbb3e970be53e30d26ac9d2c4bbd
Zheng Junjie <873216071@qq.com>
2025-01-17 14:05:57 +0800

    Imported upstream version '2.47.0' of 'upstream'

--- controller_manager/test/test_controller/test_controller.cpp
+++ controller_manager/test/test_controller/test_controller.cpp
@@ -14,7 +14,7 @@
 
 #include "test_controller.hpp"
 
-#include <limits>
+#include <memory>
 #include <string>
 
 #include "lifecycle_msgs/msg/state.hpp"
@@ -25,14 +25,13 @@ TestController::TestController()
 : controller_interface::ControllerInterface(),
   cmd_iface_cfg_{controller_interface::interface_configuration_type::NONE}
 {
-  set_first_command_interface_value_to = std::numeric_limits<double>::quiet_NaN();
 }
 
 controller_interface::InterfaceConfiguration TestController::command_interface_configuration() const
 {
   if (
-    get_lifecycle_state().id() == lifecycle_msgs::msg::State::PRIMARY_STATE_INACTIVE ||
-    get_lifecycle_state().id() == lifecycle_msgs::msg::State::PRIMARY_STATE_ACTIVE)
+    get_state().id() == lifecycle_msgs::msg::State::PRIMARY_STATE_INACTIVE ||
+    get_state().id() == lifecycle_msgs::msg::State::PRIMARY_STATE_ACTIVE)
   {
     return cmd_iface_cfg_;
   }
@@ -46,8 +45,8 @@ controller_interface::InterfaceConfiguration TestController::command_interface_c
 controller_interface::InterfaceConfiguration TestController::state_interface_configuration() const
 {
   if (
-    get_lifecycle_state().id() == lifecycle_msgs::msg::State::PRIMARY_STATE_INACTIVE ||
-    get_lifecycle_state().id() == lifecycle_msgs::msg::State::PRIMARY_STATE_ACTIVE)
+    get_state().id() == lifecycle_msgs::msg::State::PRIMARY_STATE_INACTIVE ||
+    get_state().id() == lifecycle_msgs::msg::State::PRIMARY_STATE_ACTIVE)
   {
     return state_iface_cfg_;
   }
@@ -59,39 +58,16 @@ controller_interface::InterfaceConfiguration TestController::state_interface_con
 }
 
 controller_interface::return_type TestController::update(
-  const rclcpp::Time & /*time*/, const rclcpp::Duration & period)
+  const rclcpp::Time & /*time*/, const rclcpp::Duration & /*period*/)
 {
-  if (is_async())
-  {
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000 / (2 * get_update_rate())));
-  }
-  update_period_ = period;
   ++internal_counter;
 
-  // set value to hardware to produce and test different behaviors there
-  if (!std::isnan(set_first_command_interface_value_to))
+  for (size_t i = 0; i < command_interfaces_.size(); ++i)
   {
-    command_interfaces_[0].set_value(set_first_command_interface_value_to);
-    // reset to be easier to test
-    set_first_command_interface_value_to = std::numeric_limits<double>::quiet_NaN();
-  }
-  else
-  {
-    for (size_t i = 0; i < command_interfaces_.size(); ++i)
-    {
-      if (!std::isfinite(external_commands_for_testing_[i]))
-      {
-        RCLCPP_ERROR(
-          get_node()->get_logger(),
-          "External command value for command interface '%s' is not finite",
-          command_interfaces_[i].get_name().c_str());
-        return controller_interface::return_type::ERROR;
-      }
-      RCLCPP_INFO(
-        get_node()->get_logger(), "Setting value of command interface '%s' to %f",
-        command_interfaces_[i].get_name().c_str(), external_commands_for_testing_[i]);
-      command_interfaces_[i].set_value(external_commands_for_testing_[i]);
-    }
+    RCLCPP_DEBUG(
+      get_node()->get_logger(), "Setting value of command interface '%s' to %f",
+      command_interfaces_[i].get_name().c_str(), external_commands_for_testing_[i]);
+    command_interfaces_[i].set_value(external_commands_for_testing_[i]);
   }
 
   return controller_interface::return_type::OK;
@@ -101,6 +77,36 @@ CallbackReturn TestController::on_init() { return CallbackReturn::SUCCESS; }
 
 CallbackReturn TestController::on_configure(const rclcpp_lifecycle::State & /*previous_state*/)
 {
+  auto ctrl_node = get_node();
+  if (!ctrl_node->has_parameter("command_interfaces"))
+  {
+    ctrl_node->declare_parameter("command_interfaces", std::vector<std::string>({}));
+  }
+  if (!ctrl_node->has_parameter("state_interfaces"))
+  {
+    ctrl_node->declare_parameter("state_interfaces", std::vector<std::string>({}));
+  }
+  const std::vector<std::string> command_interfaces =
+    ctrl_node->get_parameter("command_interfaces").as_string_array();
+  const std::vector<std::string> state_interfaces =
+    ctrl_node->get_parameter("state_interfaces").as_string_array();
+  if (!command_interfaces.empty() || !state_interfaces.empty())
+  {
+    cmd_iface_cfg_.names.clear();
+    state_iface_cfg_.names.clear();
+    for (const auto & cmd_itf : command_interfaces)
+    {
+      cmd_iface_cfg_.names.push_back(cmd_itf);
+    }
+    cmd_iface_cfg_.type = controller_interface::interface_configuration_type::INDIVIDUAL;
+    external_commands_for_testing_.resize(command_interfaces.size(), 0.0);
+    for (const auto & state_itf : state_interfaces)
+    {
+      state_iface_cfg_.names.push_back(state_itf);
+    }
+    state_iface_cfg_.type = controller_interface::interface_configuration_type::INDIVIDUAL;
+  }
+
   return CallbackReturn::SUCCESS;
 }
 
@@ -129,21 +135,6 @@ void TestController::set_state_interface_configuration(
   const controller_interface::InterfaceConfiguration & cfg)
 {
   state_iface_cfg_ = cfg;
-}
-
-std::vector<double> TestController::get_state_interface_data() const
-{
-  std::vector<double> state_intr_data;
-  for (const auto & interface : state_interfaces_)
-  {
-    state_intr_data.push_back(interface.get_value());
-  }
-  return state_intr_data;
-}
-
-void TestController::set_external_commands_for_testing(const std::vector<double> & commands)
-{
-  external_commands_for_testing_ = commands;
 }
 
 }  // namespace test_controller
